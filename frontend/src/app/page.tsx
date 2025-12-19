c403bd580e0b2ea3c8d1c3c8d5129c173c7c0600
Boaz Marube <boazmarube1@gmail.com>
2025-12-19 17:28:51 +0300

    Feat: How it works section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,10 +4,12 @@ import CTA from "@/components/landing/CTA"
 import Ethical from "@/components/landing/Ethical"
 import FAQ from "@/components/landing/FAQ"
 import Footer from "@/components/landing/Footer"
+import HowItWorks from "@/components/landing/HowItWorks"
 
 export default function HomePage() {
   return (
     <>
+      <HowItWorks/>
       <Ethical/>
       <FAQ/>
       <CTA/>
