fd33d8a34fe2930aa33756b7f71cda2919cceb62
Boaz Marube <boazmarube1@gmail.com>
2026-02-20 19:35:36 +0300

    Fix: Link display issue when updating an already posted job posting

--- frontend/src/components/job/JobRole.tsx
+++ frontend/src/components/job/JobRole.tsx
@@ -8,7 +8,7 @@ export function JobRole({ description }: Props) {
   
   return (
     <div className="mt-8 space-y-4 text-secondary-500 leading-relaxed">
-      <h2 className="text-lg font-semibold text-secondary-600">The Role</h2>
+      <h2 className="text-lg font-semibold text-secondary-600">About The Role</h2>
       <p>{roleOverview}</p>
     </div>
   );
